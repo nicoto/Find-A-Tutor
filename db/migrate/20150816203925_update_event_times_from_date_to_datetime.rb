84df8b0c40ff40c16961e7e75a01ac4cef5ad0bc
Nicholas Toulouse <faintsoundofwater@gmail.com>
2015-08-16 13:43:06 -0700

    change types for time_start and time_end on events table from date to datetime

--- db/migrate/20150816203925_update_event_times_from_date_to_datetime.rb
+++ db/migrate/20150816203925_update_event_times_from_date_to_datetime.rb
@@ -1,6 +1,6 @@
 class UpdateEventTimesFromDateToDatetime < ActiveRecord::Migration
   def change
     change_column(:events, :time_start, :datetime)
-    change_column(:events, :time_start, :datetime)
+    change_column(:events, :time_end, :datetime)
   end
 end
