3742b47fa7fc5dda0e471b7ae30257709e01f97b
Nicholas Toulouse <faintsoundofwater@gmail.com>
2015-08-16 14:19:01 -0700

    fixes date time, formatting still required

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816030808) do
+ActiveRecord::Schema.define(version: 20150816203925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150816030808) do
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.integer  "attendees"
-    t.date     "time_start"
-    t.date     "time_end"
+    t.datetime "time_start"
+    t.datetime "time_end"
     t.text     "description"
     t.integer  "subject_id"
     t.integer  "user_id"
