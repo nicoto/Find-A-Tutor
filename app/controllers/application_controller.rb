47f0689185e2f536531881164de2ed912469c7cb
name": null <ashton.alexander@gmail.com>
2015-08-16 11:05:32 -0700

    adds helper_methods for rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :login, :logged_in?, :logout!, :find_user, :bounce
 
   def current_user
     @user ||= User.find_by_id(session[:user_id]) if session[:user_id]
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
 
   def login(user)
     session[:user_id] = user.id
+  end
+
+  def logged_in?
+    current_user != nil
   end
 
   def logout!
@@ -23,5 +28,4 @@ class ApplicationController < ActionController::Base
   def bounce
     redirect_to users_path unless current_user
   end
-
 end
