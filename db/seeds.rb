c9a25963199bc9253fc11f0068bf9b2881ae10b9
Thrashmandicoot <Thrashmandicoot@users.noreply.github.com>
2015-08-15 20:35:42 -0700

    fixes faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,69 +5,69 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
 
 5.times do
-  User.create (first_name: Faker::Name.first_name,
+  User.create(
+               first_name: Faker::Name.first_name,
                last_name: Faker::Name.last_name,
                username: Faker::Internet.user_name,
                email: Faker::Internet.email,
-               password_hash: Faker::Internet.password(8),
+               password_hash: Faker::Internet.password,
                role: 1,
                grade: Faker::Lorem.word,
                school: Faker::Lorem.word
               )
 end
-5.times do
-  User.create (first_name: Faker::Name.first_name,
-               last_name: Faker::Name.last_name,
-               username: Faker::Internet.user_name,
-               email: Faker::Internet.email,
-               password_hash: Faker::Internet.password(8),
-               role: 0,
-               grade: Faker::Lorem.word,
-               school: Faker::Lorem.word
-              )
-end
-5.times do
-  Subject.create(name: Faker::Lorem.word)
-  User.find(rand(1..5)).groups.create(
-    name: Faker::Lorem.word,
-    description: Faker::Lorem.sentence(rand(3..5))
-  )
-end
-
-5.times do
-  time_start = Faker::Time.between(4.days.ago, Time.now, :evening)
-  time_end = time_start + 1800000
-  Group.find(rand(1..5)).events.create(name: Faker::Lorem.word,
-                attendees: rand(5..10),
-                time_start: time_start,
-                time_end: time_end,
-                description: Faker::Lorem.sentence(rand(3..5)),
-               )
-   Faker::Config.locale = 'en-US'
-   Location.create(
-    street: Faker::Address.street_address,
-    suite: Faker::Address.secondary_address,
-    city: Faker::Address.city,
-    state: Faker::Address.state,
-    zip: Faker::Address.zip,
-    name: Faker::Name.name,
-    phone: Faker::PhoneNumber.cell_phone,
-    contact: Faker::Internet.email
-   ).events << Event.last
-end
-
-5.times do
-  x = Needs.create(
-    description: Faker::Lorem.sentence(3)
-    subject_id: Subject.all.sample.id
-  )
-  Location.find(rand(1..5)).needs << x
-  User.find(rand(1..5)).needs << x
-end
-
-5.times do
-  User.all.sample.events.sample.comments.create(description: Faker::Lorem.sentence(rand(1..3)))
-end
+# 5.times do
+#   User.create (first_name: Faker::Name.first_name,
+#                last_name: Faker::Name.last_name,
+#                username: Faker::Internet.user_name,
+#                email: Faker::Internet.email,
+#                password_hash: Faker::Internet.password(8),
+#                role: 0,
+#                grade: Faker::Lorem.word,
+#                school: Faker::Lorem.word
+#               )
+# end
+# 5.times do
+#   Subject.create(name: Faker::Lorem.word)
+#   User.find(rand(1..5)).groups.create(
+#     name: Faker::Lorem.word,
+#     description: Faker::Lorem.sentence(rand(3..5))
+#   )
+# end
+#
+# 5.times do
+#   time_start = Faker::Time.between(4.days.ago, Time.now, :evening)
+#   time_end = time_start + 1800000
+#   Group.find(rand(1..5)).events.create(name: Faker::Lorem.word,
+#                 attendees: rand(5..10),
+#                 time_start: time_start,
+#                 time_end: time_end,
+#                 description: Faker::Lorem.sentence(rand(3..5)),
+#                )
+#    Faker::Config.locale = 'en-US'
+#    Location.create(
+#     street: Faker::Address.street_address,
+#     suite: Faker::Address.secondary_address,
+#     city: Faker::Address.city,
+#     state: Faker::Address.state,
+#     zip: Faker::Address.zip,
+#     name: Faker::Name.name,
+#     phone: Faker::PhoneNumber.cell_phone,
+#     contact: Faker::Internet.email
+#    ).events << Event.last
+# end
+#
+# 5.times do
+#   x = Needs.create(
+#     description: Faker::Lorem.sentence(3)
+#     subject_id: Subject.all.sample.id
+#   )
+#   Location.find(rand(1..5)).needs << x
+#   User.find(rand(1..5)).needs << x
+# end
+#
+# 5.times do
+#   User.all.sample.events.sample.comments.create(description: Faker::Lorem.sentence(rand(1..3)))
+# end
